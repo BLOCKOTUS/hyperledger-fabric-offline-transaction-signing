26873fa7edfc3d1bdb0719622c294282cfc60fa8
Daniel Febrero <febrero@me.com>
2021-03-01 20:40:56 +0100

    BLOCKOTUS/organism#63 babel better transpile

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,21 @@
 "use strict";
 
-require("core-js/modules/es.function.name.js");
-
-require("core-js/modules/es.object.to-string.js");
-
-require("core-js/modules/es.promise.js");
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.sendProposal = exports.calculateSignature = exports.hashProposal = exports.createUser = void 0;
 
-require("regenerator-runtime/runtime.js");
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
 var _fabricCommon = require("fabric-common");
 
 var _jsrsasign = require("jsrsasign");
 
-var _elliptic = require("elliptic");
-
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+var _elliptic = _interopRequireDefault(require("elliptic"));
 
 var createUser = function createUser(_ref) {
   var name = _ref.name,
@@ -49,13 +43,12 @@ var calculateSignature = function calculateSignature(_ref2) {
   var privateKeyPEM = _ref2.privateKeyPEM,
       proposalDigest = _ref2.proposalDigest;
 
-  var _KEYUTIL$getKey = _jsrsasign.KEYUTIL.getKey(privateKeyPEM),
-      prvKeyHex = _KEYUTIL$getKey.prvKeyHex; // convert the pem encoded key to hex encoded private key
+  var key = _jsrsasign.KEYUTIL.getKey(privateKeyPEM); // convert the pem encoded key to hex encoded private key
 
 
-  var ecdsaCurve = _elliptic.curves['p256'];
-  var ecdsa = new _elliptic.ec(ecdsaCurve);
-  var signKey = ecdsa.keyFromPrivate(prvKeyHex, 'hex');
+  var EC = _elliptic["default"].ec;
+  var ecdsa = new EC('p256');
+  var signKey = ecdsa.keyFromPrivate(key.prvKeyHex, 'hex');
   var sig = ecdsa.sign(Buffer.from(proposalDigest, 'hex'), signKey);
   var signature = Buffer.from(sig.toDER());
   return signature;
@@ -64,10 +57,10 @@ var calculateSignature = function calculateSignature(_ref2) {
 exports.calculateSignature = calculateSignature;
 
 var sendProposal = /*#__PURE__*/function () {
-  var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(_ref3) {
+  var _ref4 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(_ref3) {
     var _ref3$client, client, user, privateKeyPEM, _ref3$channel, channel, chaincode, fcn, args, appClient, appChannel, idx, endorsement, build_options, proposalBytes, proposalDigest, signature;
 
-    return regeneratorRuntime.wrap(function _callee$(_context) {
+    return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
